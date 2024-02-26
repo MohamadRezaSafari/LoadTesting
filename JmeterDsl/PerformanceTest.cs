3611b07d89b3b2dd2496b580839bb5504d912657
Reza <mrdev1020@gmail.com>
2024-02-26 18:14:50 +0330

    Adding Abstracta.JmeterDsl

---
+++ JmeterDsl/PerformanceTest.cs
@@ -0,0 +1,18 @@
+﻿namespace JmeterDsl;
+using static Abstracta.JmeterDsl.JmeterDsl;
+
+public class PerformanceTest
+{
+    [Fact]
+    public void LoadTest()
+    {
+        var stats = TestPlan(
+            ThreadGroup(2, 10,
+                HttpSampler("http://localhost:5105/WeatherForecast")
+            )
+        ).Run();
+
+        Assert.True(stats.Overall.SampleTimePercentile99 < TimeSpan.FromSeconds(5));
+        //Assert.That(stats.Overall.SampleTimePercentile99, Is.LessThan(TimeSpan.FromSeconds(5)));
+    }
+}
---
+++ JmeterDsl/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace JmeterDsl
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
